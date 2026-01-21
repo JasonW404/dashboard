ce3a722782e24a36914126c68e9f32412ff6e8e4
Jason Wang <jasonwong2019@outlook.com>
2026-01-21 18:50:34 +0800

    refactor: Remove todo management from dashboard store; simplify state structure

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,14 +1,10 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { DashboardSettings, TodoItem } from '@/types';
+import { DashboardSettings } from '@/types';
 
 interface DashboardState {
   settings: DashboardSettings;
-  todos: TodoItem[];
   updateSettings: (settings: Partial<DashboardSettings>) => void;
-  addTodo: (todo: TodoItem) => void;
-  toggleTodo: (id: string) => void;
-  deleteTodo: (id: string) => void;
 }
 
 export const useDashboardStore = create<DashboardState>()(
@@ -18,24 +14,9 @@ export const useDashboardStore = create<DashboardState>()(
         githubUsername: '',
         trackedRepos: [],
       },
-      todos: [],
       updateSettings: (newSettings) =>
         set((state) => ({
           settings: { ...state.settings, ...newSettings },
-        })),
-      addTodo: (todo) =>
-        set((state) => ({
-          todos: [todo, ...state.todos],
-        })),
-      toggleTodo: (id) =>
-        set((state) => ({
-          todos: state.todos.map((t) =>
-            t.id === id ? { ...t, completed: !t.completed } : t
-          ),
-        })),
-      deleteTodo: (id) =>
-        set((state) => ({
-          todos: state.todos.filter((t) => t.id !== id),
         })),
     }),
     {
